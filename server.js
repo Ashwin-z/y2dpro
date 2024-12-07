bd67d069a6b9434680090432290e824d5b05ab51
Girach-ak <akgirach5@gmail.com>
2024-12-07 12:22:10 -0700

    serr

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const ytdlCore = require('ytdl-core');
+const Core = require('ytdl-core');
 const ytdlCore = require('@distube/ytdl-core');
 const fs = require('fs');
 const os = require('os');
